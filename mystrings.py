ae15a7b94a6bf26309a8bc35ea1385b939d22990
Mine <carolinemathenge09@gmail.com>
2023-08-18 20:17:55 +0300

    first commit

---
+++ mystrings.py
@@ -0,0 +1,35 @@
+first_name='John'
+last_name="Ng'ang'a"
+
+full_name= first_name +" "+ last_name
+#print(full_name)
+#print(full_name.lower())
+#print(full_name)
+#print(full_name.upper())
+#operations or methods are available to us to use so that we can manipulate. use a . to access the methods.
+#Operations in python
+#print(full_name.casefold)
+#print(full_name.capitalize())
+#print(full_name.count(""))-integers
+#print(full_name.find("a"))
+#print(full_name.index("x"))
+#print(full_name.split())
+#print(full_name.strip(""))
+#print(full_name.islower())-bolean
+
+#dealings with numbers that are strings
+num1= "7"
+num2= "8"
+#print(num1.isdecimal())- bolean
+t= int(num1) + int(num2)
+print(t)
+
+#indexing and slicing
+#This is used when you want to get a part of a string
+#Indexing- Access one character.
+#Slicing- Access multiple characters
+#print(full_name[-11])- indexing
+#part1- index where you want to start
+#part2- count where you want to stop.
+#print(full_name[5:13])- ng'ang'a
+#print(len("john"))- checking the length
